93ec7b710f33db8c027296e28254d82d3715cbd9
cowen27 <calvin.owen27@gmail.com>
2024-07-08 13:06:51 -0700

    fixed callbacks and index for engine

--- src/state-objects/VectorRace.js
+++ src/state-objects/VectorRace.js
@@ -1,6 +1,20 @@
+import GameController from "../engine/GameController.js"
+
 "use strict"
 
 export default class VectorRace {
     // holds game state
     // state machine
+
+    constructor() {
+        this.game = new GameController();
+    }
+
+    init() {
+    }
+
+    run() {
+        // run game
+        this.game.run();
+    }
 }
