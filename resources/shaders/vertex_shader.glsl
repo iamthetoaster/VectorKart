fa9944b716199324ef1570949683d5975c8708df
Toaster <14080501+iamthetoaster@users.noreply.github.com>
2024-07-08 14:01:13 -0700

    Solving merge conflicts

--- resources/shaders/vertex_shader.glsl
+++ resources/shaders/vertex_shader.glsl
@@ -9,11 +9,15 @@
 
     uniform mat4 u_matrix;
 
+    out vec3 v_color;
+
+    uniform mat4 u_matrix;
+
     // all shaders have a main function
     void main() {
 
       // gl_Position is a special variable a vertex shader
       // is responsible for setting
       gl_Position = u_matrix * a_position;
-      v_normal = a_normal;
+      v_color = a_position.xyz;
     }
