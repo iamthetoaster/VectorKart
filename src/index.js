43e2064f88946edf6d0d0ae10e97d7de63914267
cowen27 <calvin.owen27@gmail.com>
2024-07-09 11:03:08 -0700

    removed index.js (empty)

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { resizeCanvasToDisplaySize, parseObjText, createShader, createProgram } from "./webgl_utils.js"
-import { mat4, radToDeg, degToRad } from "./math_utils.js"
-import { render } from "./render.js"
-
-"use strict";
