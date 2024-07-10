fd694ee56d5194c6262f932fe69460c5e45b337c
cowen27 <calvin.owen27@gmail.com>
2024-07-10 09:54:25 -0700

    beginning to set up RenderObject

--- src/state-objects/car.js
+++ src/state-objects/car.js
@@ -1,43 +1,5 @@
 import { render } from "../render.js";
+import RenderObject from "./RenderObject.js";
 
-export default class Car {
-    constructor() {
-        this.position = { x: 0, y: 0, z: 0 };
-        this.rotation = { x: 0, y: 0, z: 0 };
-        this.scale = { x: 1, y: 1, z: 1 };
-        this.velocity = { x: 0, y: 0, z: 0 };
-    }
-
-    setPosition(x, y, z) {
-        this.position = { x, y, z };
-    }
-
-    setRotation(x, y, z) {
-        this.rotation = { x, y, z };
-    }
-
-    setScale(x, y, z) {
-        this.scale = { x, y, z };
-    }
-
-    setVelocity(vx, vy, vz) {
-        this.velocity = { x: vx, y: vy, z: vz };
-    }
-
-    updateTransform() {
-        // Update the WebGL transformation matrices based on the car's state
-        if (render.models.car) {
-            render.models.car.transform.translation = [this.position.x, this.position.y, this.position.z];
-            render.models.car.transform.rotation = [this.rotation.x, this.rotation.y, this.rotation.z];
-            render.models.car.transform.scale = [this.scale.x, this.scale.y, this.scale.z];
-            render.draw(); // Redraw the scene with updated transformations
-        }
-    }
-
-    printState() {
-        console.log(`Position: (${this.position.x}, ${this.position.y}, ${this.position.z})`);
-        console.log(`Velocity: (${this.velocity.x}, ${this.velocity.y}, ${this.velocity.z})`);
-        console.log(`Rotation: (${this.rotation.x}, ${this.rotation.y}, ${this.rotation.z})`);
-        console.log(`Scale: (${this.scale.x}, ${this.scale.y}, ${this.scale.z})`);
-    }
+export default class Car extends RenderObject {
 }
