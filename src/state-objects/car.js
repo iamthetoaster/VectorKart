a6e18d454546b5ac110f1167c2c0408c772ad7b2
cowen27 <calvin.owen27@gmail.com>
2024-07-10 11:30:03 -0700

    fixed Car styling

--- src/state-objects/car.js
+++ src/state-objects/car.js
@@ -3,26 +3,25 @@ import RenderObject from "./RenderObject.js";
 import { degToRad } from "../math_utils.js";
 
 export default class Car extends RenderObject {
-    constructor() {
-        super("car");
+  constructor() {
+    super("car");
+    this.setVelocity(0, 0, 0);
+    this.setAcceleration(0, 0, 0);
+  }
 
-        this.setVelocity(0, 0, 0);
-        this.setAcceleration(0, 0, 0);
-    }
+  setVelocity(vx, vy, vz) {
+    this._velocity = { x: vx, y: vy, z: vz };
+  }
 
-    setVelocity(vx, vy, vz) {
-        this._velocity = { x: vx, y: vy, z: vz };
-    }
+  getVelocity() {
+    return this._velocity;
+  }
 
-    getVelocity() {
-        return this._velocity;
-    }
+  setAcceleration(x, y, z) {
+    this._acceleration = { x: x, y: y, z: z };
+  }
 
-    setAcceleration(x, y, z) {
-        this._acceleration = { x: x, y: y, z: z };
-    }
-
-    getAcceleration() {
-        return this._acceleration;
-    }
+  getAcceleration() {
+    return this._acceleration;
+  }
 }
