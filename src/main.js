8905553fe27fd267f3274ddc53fb680dd63b82e9
cowen27 <calvin.owen27@gmail.com>
2024-07-10 11:24:20 -0700

    clean main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
 import GameController from "./engine/GameController.js";
 
 "use strict";
-
-// const canvasId = "c";
-// const game = new GameController();
-
-// const canvas = document.getElementById(canvasId);
-// canvas.addEventListener("click", game.clickUpdate);
-
-// game.run();
