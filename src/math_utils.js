6f8f52baba389b7acedac717a28be717a6946609
Toaster <14080501+iamthetoaster@users.noreply.github.com>
2024-07-11 12:18:57 -0700

    Removed duplicate function

--- src/math_utils.js
+++ src/math_utils.js
@@ -214,19 +214,6 @@ const mat4 = {
     ];
   },
 
-  orthographic: function(left, right, bottom, top, near, far) {
-    return [
-      2 / (right - left), 0, 0, 0,
-      0, 2 / (top - bottom), 0, 0,
-      0, 0, 2 / (near - far), 0,
-
-      (left + right) / (left - right),
-      (bottom + top) / (bottom - top),
-      (near + far) / (near - far),
-      1,
-    ];
-  },
-
   lookAt: function(cameraPosition, target, up) {
     let zAxis = vec3.normalize(
       vec3.subtract(cameraPosition, target));
