c4c1f02835fc33a0be9fca0144d0915ebaeb0fb7
Gobefang <gobefang@gmail.com>
2024-07-12 00:19:09 -0700

    Turned Map into class

--- src/map.js
+++ src/map.js
@@ -1,93 +1,130 @@
-function genMap(x, y){
-    let map = [];
-    
-    for(let i = 0; i < y; i++){
-        map[i] = [];
-        for(let j = 0; j < x; j++){
-            map[i][j] = 0;
+
+function Map(x, y){
+    const UNASSIGNED = 0;
+    const WALL = 1;
+    const TRACK = 2;
+
+    this.map = [];    
+    this.x = x;
+    this.y = y;
+    for(let i = 0; i < x; i++){
+        this.map[i] = [];
+        for(let j = 0; j < y; j++){
+            this.map[i][j] = 0;
         }
     }
-    return map;
-}
+    
+    //map visualizer in th 4th quadrant
+    this.visMap = function() {
+        let s = ""; // a string to represent the array
+        for(let yValues = 0; yValues < this.y; yValues++){
+            s = "";
+            for(let xValues = 0; xValues < this.x; xValues++){
+                
+                if(this.map[xValues][yValues] == UNASSIGNED){
+                    s = s.concat("O" + "&nbsp" + "&nbsp" + "&nbsp"); //O represents an undefined value
+                }
+    
+                else if(this.map[xValues][yValues] == WALL){
+                    s = s.concat("X" + "&nbsp" + "&nbsp" + "&nbsp");// X represents a wall
+                }
+    
+                else if(this.map[xValues][yValues] == TRACK){
+                    s = s.concat("Q" + "&nbsp" + "&nbsp" + "&nbsp");// Q represents track space
+                }
 
-
-//console.log(map);
-
-//horizontal track editor
-function hLine(map, x1, x2, y1){
-    while(x1 <= x2){
-        map[y1][x1] = 1;
-        x1++;
-    }
-}
-
-//vertical track editor
-function vLine(map, y1, y2, x1){
-    while(y1<=y2){
-        map[y1][x1] = 1;
-        y1++;
-    }
-}
-
-
-function fill(map, x, y){
-    let f = 0;
-    for(let i = 0; i<y; i++){
-        for(let j = 0; j<x; j++){
-            if(f == 1 && map[i][j] == 0){
-                map[i][j] = 2;
+                else{
+                    s = s.concat("?" + "&nbsp" + "&nbsp" + "&nbsp");// ? for mistakes lol
+                }
             }
-            if(f == 0 && map[i][j] == 1){
-                f = 1;
-            }
-
-            else if(f == 1 && map[i][j] == 1){
-                f = 0;
-            }
+    
+            document.write(s);
+            document.write("<br>");
             
         }
-    }
-}
+    };
 
-//map visualizer in the 4th quadrant
-function visMap(map, x , y){
-    let s = "";
-    for(let i = y-1; i >= 0; i--){
-        s = "";
-        for(let j = 0; j < x; j++){
-            if(map[i][j] == 0){
-                s = s.concat("O" + "&nbsp" + "&nbsp" + "&nbsp");
-            }
+    //horizontal track editor
+    this.hLine = function(xStart, xEnd, yLevel){
+        while(xStart <= xEnd){
+            this.map[xStart][yLevel] = WALL;
+            xStart++;
+        }
+    };
 
-            else if(map[i][j] == 1){
-                s = s.concat("X" + "&nbsp" + "&nbsp" + "&nbsp");
-            }
+    //vertical track editor
+    this.vLine = function(yStart, yEnd, xLevel){
+        while(yStart <= yEnd){
+            this.map[xLevel][yStart] = WALL;
+            yStart++;
+        }
+    };
 
-            else{
-                s = s.concat("Q" + "&nbsp" + "&nbsp" + "&nbsp");
+    //fills in between WALLS with TRACK
+    this.fill = function(){
+        let alternateWhenWall = false;
+        for(let xValues = 0; xValues<this.x; xValues++){
+            for(let yValues = 0; yValues<this.y; yValues++){
+                if(alternateWhenWall == true && this.map[xValues][yValues] == UNASSIGNED){
+                    this.map[xValues][yValues] = TRACK;
+                }
+                if(alternateWhenWall == false && this.map[xValues][yValues] == WALL){
+                    alternateWhenWall = true;
+                }
+    
+                else if(alternateWhenWall == true && this.map[xValues][yValues] == WALL){
+                    alternateWhenWall = false;
+                }
+                
             }
         }
+    };
 
-        document.write(s);
-        document.write("<br>");
-        
+    //create positive diagonal line
+    this.diagonalUp = function(xStart, xEnd, yStart, yEnd){
+        while(xStart < xEnd && yStart < yEnd){
+            this.map[xStart][yStart] = WALL;
+            xStart++;
+            yStart++;
+        }
+    };
+
+    //creat negative diagonal line
+    this.diagonalDown = function(xStart, xEnd, yStart, yEnd){
+        while(xStart < xEnd && yStart > yEnd){
+            this.map[xStart][yStart] = WALL;
+            xStart++;
+            yStart--;
+        }
     }
+/*
+    this.Circle = function(radius, xCenter, yCenter){
+        this.map.diagonalUp(xCenter - radius, xCenter, yCenter, yCenter + radius);
+        this.map.diagonalUp(xCenter, xCenter + radius, yCenter - radius, yCenter);
+        this.map.diagonalDown(xCenter - radius, xCenter, yCenter, yCenter - radius);
+        this.map.diagonalDown(xCenter, xCenter + radius, yCenter + radius, yCenter);
+    };*/
 }
-
 //main 
+
+
 let x = 24;
 let y = 10;
-let map = genMap(x, y);
+let nemo = new Map(x,y); //nemo cuz why not
 
-hLine(map, 4, 19, 3);
-hLine(map, 4, 19, 6);
-vLine(map, 3, 6, 19);
-vLine(map, 3, 6, 4);
+//Rectangle
 
-hLine(map, 0, 23, 0);
-hLine(map, 0, 23, 9);
-vLine(map, 0, 9, 0);
-vLine(map, 0, 9, 23);
+nemo.hLine(4, 19, 3);
+nemo.hLine(4, 19, 6);
+nemo.vLine(3, 6, 4);
+nemo.vLine(3, 6, 19);
 
-fill(map, x, y);
-visMap(map, x, y);
+nemo.hLine(0, 23, 0);
+nemo.hLine(0, 23, 9);
+nemo.vLine(0, 9, 0);
+nemo.vLine(0, 9, 23);
+
+
+nemo.fill();
+
+nemo.visMap();
