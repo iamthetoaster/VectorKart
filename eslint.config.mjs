75ee52738d0723977ca5a24258b99bbd71d5b516
BuzyBeeC <brianc.gde@gmail.com>
2024-07-13 11:34:38 -0700

    Configure ESLint and Jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,21 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin-js";
+import unicorn from "eslint-plugin-unicorn";
+import pluginJest from "eslint-plugin-jest";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "@stylistic/js/indent": ["error", 2],
+      "@stylistic/js/quotes": ["error", "single"],
+      "@stylistic/js/array-element-newline": ["error", "consistent"]
+    }
+  },
   pluginJs.configs.recommended,
+  stylistic.configs["all-flat"],
+  unicorn.configs["flat/recommended"],
+  pluginJest.configs["flat/recommended"],
 ];
