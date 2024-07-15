ff2051529978d27d8a276544f05b611fb294abe2
CoolCaper <31525658+CoolCaper@users.noreply.github.com>
2024-07-14 17:52:22 -0700

    push

--- src/vector.js
+++ src/vector.js
@@ -1,10 +1,6 @@
 class vector {
-<<<<<<< Updated upstream
-    constructor(a_x=0,a_y=0,color=[1,1,1,1],turn,dist) {
-=======
     constructor(a_x=0,a_y=0,color=[1,1,1,1],turn,prev=null) {
         //assume
->>>>>>> Stashed changes
         
         this.color = color;
         //coordinates for point a 
@@ -13,16 +9,13 @@ class vector {
         //coordinates for point b
         this.b_x = a_x; //b_x and b_y will be determined by user input, so they can just be made the same as point a by default
         this.b_y = a_y;
-<<<<<<< Updated upstream
         this.time = turn;
         this.dist = dist
 
-=======
         this.dist = this.get_distance()
         this.velocity = dist / turn
         this.prev = prev
         this.acc = this.get_accelleration()
->>>>>>> Stashed changes
     }
     //what will my vector need 
     get_distance() {
@@ -31,15 +24,23 @@ class vector {
         let y_len = this.b_y - this.a_y;
         let z_sqrd = Math.pow(x_len, 2) + Math.pow(y_len, 2);
         return Math.sqrt(z_sqrd);
-    }
-
-    get_accelleration() {
         if (this.prev == null) {
             return;
         }
         //get previous velocity, 
         return this.prev.velocity + this.velocity
 
+    }
+    get_triangle() { //get specs for rendering triangle. Might not want to render it in here though.
+        var old_vel = this.prev.velocity; 
+        var numerator = Math.pow(this.acc, 2) + Math.pow(old_vel, 2) - Math.pow(this.velocity, 2)
+        var denominator = 2 * old_vel * this.velocity 
+        var p3 = Math.acos(numerator / denominator)
+
+        //alright coordinates
+        //0 0 0
+        //acceleration 0 0
+        //trigonometry for the rest
     }
     render() {
         //velocity triangle for gabe
