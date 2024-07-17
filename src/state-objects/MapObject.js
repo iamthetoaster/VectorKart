9f3be801556922d222013b8ea44e3124b8d5cf74
Calvin Owen <calvin.owen27@gmail.com>
2024-07-17 16:10:57 -0700

    changed canvas dimensions to 750x750px

--- src/state-objects/MapObject.js
+++ src/state-objects/MapObject.js
@@ -28,6 +28,7 @@ export default class MapObject extends GameObject3D {
 
         this.scale = new Vector3(10, 10, 10);
         this.rotation = 0;
+        this.position = new Vector3(0, 0, 0);
     }
 
 
