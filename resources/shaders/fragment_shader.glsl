cfbc0eb29b89bef5994921c265b04ec87d60c721
CoolCaper <31525658+CoolCaper@users.noreply.github.com>
2024-07-21 18:08:53 -0700

    getting stuff from last quarter

--- resources/shaders/fragment_shader.glsl
+++ resources/shaders/fragment_shader.glsl
@@ -11,4 +11,38 @@
     void main() {
       // Just set the output to a constant reddish-purple
       outColor = vec4((normalize(v_normal.xyz) + vec3(1, 1, 1)) * 0.5, 1);
+
+      
+      vec3 norm = normalize(v_Normal);
+
+      vec3 ambient = vec3(gl_FragColor) * 0.1;    
+
+
+      vec3 lightVector = vec3(v_VertPos) - u_lightPosition;
+      vec3 L = normalize(lightVector);
+      float NdotL = max(dot(norm, L), 0.0);
+
+      vec3 diffuse = vec3(gl_FragColor) * NdotL*.8;
+
+
+      vec3 R = reflect(-L, norm);
+      vec3 E = normalize(u_Eye - vec3(v_VertPos));  
+      vec3 specular = vec3(gl_FragColor) * pow(max(dot(E,R), 0.0), 10.0);
+
+      gl_FragColor = vec4((diffuse + ambient + specular) * u_lightRGB, 1.0);
+
+
+      //make a new light vector for spotlight. Position isn't a variable so can just put in numbers 
+      vec3 spotlightVector = vec3(3.9, .2, -3);
+
+      vec3 spotlightTarget = vec3(0, -.75, 0);
+
+      vec3 w = normalize(spotlightVector - spotlightTarget);
+      float ndotSL = max(dot(w, norm), 0.0);
+
+      vec3 spotlightSpecular = ndotSL * vec3(gl_FragColor);      
+      vec3 spotlightDiffuse = vec3(gl_FragColor) * ndotSL*.8;
+      vec3 spotlightAmbient = vec3(gl_FragColor) * 0.2;
+
+      gl_FragColor = vec4(diffuse + spotlightDiffuse + ambient + specular + spotlightSpecular, 1.0);
     }
