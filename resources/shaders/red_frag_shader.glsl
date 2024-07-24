fb5a9d3d9676ca3dc54d8305a8032995c6c08e3a
cowen27 <calvin.owen27@gmail.com>
2024-07-23 17:23:41 -0700

    added red shader

---
+++ resources/shaders/red_frag_shader.glsl
@@ -0,0 +1,15 @@
+    #version 300 es
+
+    // fragment shaders don't have a default precision so we need
+    // to pick one. highp is a good default. It means "high precision"
+    precision highp float;
+
+    // we need to declare an output for the fragment shader
+    in vec4 v_normal;
+    out vec4 outColor;
+
+    void main() {
+      // Just set the output to a constant reddish-purple
+      // outColor = vec4((normalize(v_normal.xyz) + vec3(1, 0, 0)) * 0.5, 1);
+      outColor = vec4((normalize(v_normal.xyz) + vec3(1, -0.75, 0.125)), 1);
+    }
